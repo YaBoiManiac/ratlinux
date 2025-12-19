6931ef7d152a1d91a04fb27f4d08c4103d61d975
Richard <41370482+YaBoiManiac@users.noreply.github.com>
2025-12-19 18:04:59 +1100

    Update all.sh

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,4 +1,3 @@
 run_logged $OMARCHY_INSTALL/post-install/pacman.sh
-run_logged omarchy-install-vscode
 source $OMARCHY_INSTALL/post-install/allow-reboot.sh
 source $OMARCHY_INSTALL/post-install/finished.sh
